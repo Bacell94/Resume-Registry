c564a54adecdbed0c9975e39e9f4b18a962d896d
Bassel saleh <bacell94@gmail.com>
2022-10-25 18:37:55 +0300

    modified pdo.php

--- pdo.php
+++ pdo.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=127.0.0.1;port=3306;dbname=resume', 
-   'basel', '1234');
+$pdo = new PDO('mysql:host=host;port=port;dbname=yourdb', 
+   'name', 'pass');
 // See the "errors" folder for details...
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
