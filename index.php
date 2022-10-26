9d08e3edb0671cc0d718cfed2202a2dba0799e5b
Bassel saleh <bacell94@gmail.com>
2022-10-26 14:24:31 +0300

    refactor display text if no entries are found

--- index.php
+++ index.php
@@ -3,6 +3,13 @@ require_once "pdo.php";
 require_once "util.php";
 session_start();
 
+if(isset($_SESSION['user_id'])){
+    $stmt = $pdo->query("SELECT * FROM profile where user_id =".$_SESSION['user_id']);
+    $profile = array();
+    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+        $profile[] = $row;
+    }
+}
 
 ?>
 <!DOCTYPE html>
@@ -20,40 +27,38 @@ session_start();
 
     flashMessages();
 
-    if(!isset($_SESSION['name'])){
+    if(!isset($_SESSION['user_id'])){
         
         echo "<p><a href='login.php'>Please log in</a></p>";
+
     }else{
+
         echo "<p><a href='add.php'>Add New Entry</a></p>";
 
-        
-    	$stmt = $pdo->query("SELECT * FROM profile where user_id =".$_SESSION['user_id']);
-        
-        $entry = false;
+        if(count($profile) !== 0){
 
-        echo('<table border="1">');
-        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            echo('<table border="1">');
+            foreach($profile as $row){
+    
+                echo "<tr><td>";
+                echo('<a href="view.php?profile_id='.$row['profile_id'].'">'.htmlentities($row['first_name'])." ".htmlentities($row['last_name']).'</a>');
+                echo("</td><td>");
+                echo(htmlentities($row['headline']));
+                echo("</td><td>");
+                echo(htmlentities($row['summary']));
+                echo("</td><td>");
+                echo('<a href="edit.php?profile_id='.$row['profile_id'].'">Edit</a> / ');
+                echo('<a href="delete.php?profile_id='. $row['profile_id'].'">Delete</a>');
+                echo("</td></tr>\n");       
 
-            $entry = true;
+            }
+            echo '</table>';
 
-            echo "<tr><td>";
-            echo('<a href="view.php?profile_id='.$row['profile_id'].'">'.htmlentities($row['first_name'])." ".htmlentities($row['last_name']).'</a>');
-            echo("</td><td>");
-            echo(htmlentities($row['headline']));
-            echo("</td><td>");
-            echo(htmlentities($row['summary']));
-            echo("</td><td>");
-            echo('<a href="edit.php?profile_id='.$row['profile_id'].'">Edit</a> / ');
-            echo('<a href="delete.php?profile_id='. $row['profile_id'].'">Delete</a>');
-            echo("</td></tr>\n");
-            
-            
-        }
-        echo '</table>';
+        }else{
 
-        if(!$entry){
             echo "<p>Not entries found</p>";
         }
+
         echo "<p><a href='logout.php'>Logout</a></p>";
     }
     ?>
